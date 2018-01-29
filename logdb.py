2a81fc7188729a7e6ed4f10edcdad909aef59b61
Yaz Khoury <yaz.khoury@gmail.com>
2018-01-28 22:14:09 -0500

    feat: add query script

--- logdb.py
+++ logdb.py
@@ -14,18 +14,18 @@ def run_query(query_command):
 
 def get_popular_article():
     """Get top 3 most popular articles"""
-    query_command = ""
+    query_command = "SELECT * from popular_posts LIMIT 3"
     query_data = run_query(query_command)
     return query_data
 
 def get_popular_authors():
     """Get top 3 most popular authors"""
-    query_command = ""
+    query_command = "SELECT * from popular authors LIMIT 3"
     query_data = run_query(query_command)
     return query_data
 
 def find_anomaly():
     """Gets date where 1% of requests return error"""
-    query_command = ""
+    query_command = "SELECT day FROM error_log WHERE error_percent > 1.0"
     query_data = run_query(query_command)
     return query_data
